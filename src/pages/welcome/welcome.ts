724faf3a11340444c2d44d9978852fe65292a6bf
PrasanthJayaraman <prasanthj@outlook.com>
2017-11-12 01:19:01 +0530

    Added welcome

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 import { Home } from '../home/home';
 /**
  * Generated class for the WelcomePage page.
@@ -15,19 +17,54 @@ import { Home } from '../home/home';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public user : FormGroup;
+  
+  alert(title, content) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: content,
+      buttons: ['Okay']
+    });
+    alert.present();
+  }
+
+  constructor(public formBuilder: FormBuilder, public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+    this.user = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WelcomePage');
   }
 
-  login(): void {
+  login(form) {
+    var data = this.user.value
+    if(data.name == ""){
+      return this.alert("Alert", "Please enter your name");
+    } else if(data.name && data.name.length < 3){
+      return this.alert("Alert", "Please enter a valid name");
+    }
+
+    if (!data.email) {
+      return this.alert("Alert", "Please enter your email");
+    } else if (data.email && !this.validateEmail(data.email)) {
+      return this.alert("Alert", "Please enter a valid email");
+    }
+
+    if (!data.phone) {
+      return this.alert("Alert", "Please enter your phone number");
+    } else if (data.phone && data.phone.length < 10) {
+      return this.alert("Alert", "Please enter a valid phone number");
+    }
     this.navCtrl.push(Home);
   }
 
-  signup(): void {
-    this.navCtrl.push(Home);
-  }
+  validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }  
 
 }
