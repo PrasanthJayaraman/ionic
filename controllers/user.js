35fb576e02ad66414a2e1ed2dcffc042b79e0830
PrasanthJayaraman <prasanthj@outlook.com>
2017-11-18 20:22:02 +0530

    cors added

--- controllers/user.js
+++ controllers/user.js
@@ -42,7 +42,37 @@ exports.getStarted = function(req, res, next){
 }
 
 exports.socialLogin = function(req, res, next){
-    
+    var data = req.body.user;
+
+    if (!data || Object.keys(data).length == 0) {
+        return res.status(400).send({
+            message: "Invalid credentials entered"
+        });
+    } else {
+        if (!data.email) {
+            return res.status(400).send({
+                message: "Invalid Email Address"
+            });
+
+        } else if (!data.name) {
+            return res.status(400).send({
+                message: "Invalid Name"
+            });
+        }
+
+        data.authKey = common.rand();
+
+        User.findOneAndUpdate({ email: data.email }, data, { upsert: true, new: true }, function (err, user) {
+            if (err) {
+                return res.status(400).send({
+                    message: "Server is busy, Please try again!"
+                });
+            } else {
+                return res.status(200).send(user);
+            }
+        });
+
+    }
 }
 
 exports.getUser = function(req, res, next){
