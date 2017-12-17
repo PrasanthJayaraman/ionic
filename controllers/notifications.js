100867801db340b68d628ca9dc218b3ca4e7d1fe
PrasanthJayaraman <root@personal-site.c.personal-site-182012.internal>
2017-12-17 14:45:16 +0000

    Added new push message

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -1,27 +1,70 @@
-var FCM = require('fcm-node')
+var admin = require("firebase-admin");
 var path = require('path');
 var jsonFile = path.join(__dirname, '..', 'jobswala-7a564-firebase-adminsdk-2i17m-5fe150b5fa.json');
-var serverKey = require(jsonFile); //put the generated private key path here    
-
-var fcm = new FCM(serverKey)
+admin.initializeApp({
+    credential: admin.credential.cert(jsonFile)
+});
 
 exports.send = function(tokens, title, body){
-    var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
-        registration_ids: tokens,        
-        notification : {}      
-    }
-    if(title){
-        message.notification.title = title;        
-    }
-    if(body){
-        message.notification.body = body;
-    }
+   var registrationTokens = tokens;
+   var payload = {
+       notification : {
+        "sound": "default",
+        "icon": "fcm_push_icon"
+       }
+   };
 
-    fcm.send(message, function (err, response) {        
-        if (err) {
-            console.log("Something has gone wrong!", err);
-        } else {
-            console.log("Successfully sent with response: ", response)
-        }
+   if (title) {
+       payload.notification.title = title;
+   }
+   if (body) {
+       payload.notification.body = body;
+   }   
+
+   console.log(payload);
+
+   admin.messaging().sendToDevice(registrationTokens, payload)
+    .then(function (response) {
+        // See the MessagingDevicesResponse reference documentation for
+        // the contents of response.
+        console.log("Successfully sent message:", response);
     })
+    .catch(function (error) {
+        console.log("Error sending message:", error);
+    });
 }
+
+
+/**
+ * 
+ *
+ var FCM = require('fcm-node')
+ var path = require('path');
+ var jsonFile = path.join(__dirname, '..', 'jobswala-7a564-firebase-adminsdk-2i17m-5fe150b5fa.json');
+ var serverKey = require(jsonFile); //put the generated private key path here    
+
+ var fcm = new FCM(serverKey)
+
+ exports.send = function (tokens, title, body) {
+     var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+         registration_ids: tokens,
+         notification: {},
+         priority: '10'
+     }
+     if (title) {
+         message.notification.title = title;
+     }
+     if (body) {
+         message.notification.body = body;
+     }
+     console.log(message);
+     fcm.send(message, function (err, response) {
+         if (err) {
+             console.log("Something has gone wrong!", err);
+         } else {
+             console.log("Successfully sent with response: ", response)
+         }
+     })
+ }
+
+ */
